84683cc5e1c01b9bbc5241440d570f2998009a2f
moayman19 <100526008+moayman19@users.noreply.github.com>
2022-10-27 15:53:02 +0200

    Add files via upload

---
+++ MoiveApp/MoiveApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoiveApp
+//
+//  Created by MohammedAyman on 10/27/22.
+//  Copyright © 2022 MohammedAyman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
